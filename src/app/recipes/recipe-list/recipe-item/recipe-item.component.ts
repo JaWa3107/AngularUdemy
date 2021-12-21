58136734e36eb9babb5557ec529eaa4c9ff4a296
Jannik Walter <jannik_walter@online.de>
2021-12-21 14:57:14 +0100

    UPDATE (Abschnitt 12)

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -9,16 +8,13 @@ import { RecipeService } from '../../recipe.service';
 })
 export class RecipeItemComponent implements OnInit {
  @Input() recipe: Recipe;
- 
+ @Input() index: number;
 
-  constructor(private recipeService: RecipeService) {}
 
   ngOnInit(): void {
     
   }
 
-  onSelected() {
-    this.recipeService.recipeSelected.emit(this.recipe);
-  }
+  
 
 }
